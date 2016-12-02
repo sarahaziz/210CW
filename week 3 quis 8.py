83c835f35e068564cccbd475d4d24c9d31a36769
sarahaziz <sarah.abdulaziz@hotmail.com>
2016-12-02 20:05:56 +0000

    Update week 3 quis 8.py

--- week 3 quis 8.py
+++ week 3 quis 8.py
@@ -1,17 +1,16 @@
 def remove(string):
-    if len(streing)==0:#if the lenth of string you pass is 0 then return that 
-        return string # the lenth function len will give the lenth of the string 
+    if len(streing)==0: 
+        return string  
     else:
-        newstring=string[1:len(string)+1]#1-string[1:len(string)+1] will give all the characters
-        #2- from first character onwords 
+        newstring=string[1:len(string)+1]
         firstletter=string[0]
 
         if firstletter in 'aeiouAEIOU':
 
-            return remove(newstring)#recursion definsation
+            return remove(newstring)
 
         else :
-            return firstletter + remove(newstring)# recursion
+            return firstletter + remove(newstring)
 
 
 print remove('Beautiful')
