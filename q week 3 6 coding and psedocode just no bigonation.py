f5c5531eebacf7f145e6219a2073a43b5c8a2d65
sarahaziz <sarah.abdulaziz@hotmail.com>
2016-12-02 20:11:49 +0000

    Update q week 3 6 coding and psedocode just no bigonation.py

--- q week 3 6 coding and psedocode just no bigonation.py
+++ q week 3 6 coding and psedocode just no bigonation.py
@@ -1,7 +1,5 @@
-sentence='Thus Is Awesome' # take the intial string 
-words=sentence.split()# split method will make a list of all the words togather 
+sentence='Thus Is Awesome' 
+words=sentence.split() 
 print words 
-sentence_rev=''.join(reversed(words)) #-1to reverse the list make the use of reversed key word
-#2- so that the words in the list will be reversed and we are joing them with the join key word in
-#3- the empty string  '   '
+sentence_rev=''.join(reversed(words)) 
 print sentence_rev
